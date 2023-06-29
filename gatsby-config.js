836296e87b4f3d30f653ba8887130e2e5f466b69
Brandon <fex123griffin@gmail.com>
2023-06-29 11:25:29 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `project1`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
